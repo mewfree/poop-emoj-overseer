60eab034a7e520f06db442cb893f3ba4774c9895
mewfree <damien.gonot@gmail.com>
2015-11-23 00:28:43 -0500

    First version

---
+++ README.md
@@ -0,0 +1,6 @@
+# Poop emoji overseer
+Displays last 10 tweets containing 💩
+
+Real-time updates thanks to socket.io
+
+TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN_KEY and TWITTER_ACCESS_TOKEN_SECRET have to be set through environment variables in order to use Twitter's streaming API
---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express')
+  , app = express()
+  , server = require('http').Server(app)
+  , io = require('socket.io')(server)
+  , stylus = require('stylus')
+  , Twitter = require('twitter');
+
+// Express config
+var port = process.env.PORT || 3000;
+server.listen(port+1); // io port
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(stylus.middleware ({
+  src: __dirname + '/views',
+  dest: __dirname + '/public'
+}));
+app.use(express.static(__dirname + '/public'));
+
+// Routes
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+// Twitter
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+// io
+client.stream('statuses/filter', {track: '💩'}, function(stream) {
+  io.on('connection', function (socket) {
+    stream.on('data', function(tweet) {
+      // console.log(tweet);
+      if (!tweet.retweeted_status) {
+        socket.emit('tweet', {
+          'text': tweet.text,
+          'name': tweet.user.name
+        });
+      }
+    });
+
+    stream.on('error', function(error) {
+      throw error;
+    });
+  });
+});
+
+// Start server
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+
+});
